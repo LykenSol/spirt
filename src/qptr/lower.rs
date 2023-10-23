b00084eb3599aa556568c5d1723096f7538a4827
Eduard-Mihai Burtescu <eddyb@lyken.rs>
2023-10-23 10:12:56 +0300

    Add `DataInstKind::Scalar` for pure scalars->scalars ops.

--- src/qptr/lower.rs
+++ src/qptr/lower.rs
@@ -616,6 +616,7 @@ impl LowerFromSpvPtrInstsInFunc<'_> {
             NodeKind::Select(_)
             | NodeKind::Loop { .. }
             | NodeKind::ExitInvocation(_)
+            | DataInstKind::Scalar(_)
             | DataInstKind::FuncCall(_)
             | DataInstKind::QPtr(_) => return,
 
