9743a0dcb30ff4ae2e3c649c8f3913f8da0bbcef
Eduard-Mihai Burtescu <eddyb@lyken.rs>
2024-09-11 07:14:32 +0300

    Support attributes on `Node`s (not just their output values).

--- src/spv/lower.rs
+++ src/spv/lower.rs
@@ -1559,6 +1559,7 @@ impl Module {
                             let block_node = func_def_body.nodes.define(
                                 &cx,
                                 NodeDef {
+                                    attrs: AttrSet::default(),
                                     kind: NodeKind::Block { insts: EntityList::empty() },
                                     outputs: SmallVec::new(),
                                 }
