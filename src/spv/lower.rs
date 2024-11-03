44f06600f50f8cfd5b7ae543ecfc81761341a7f7
Eduard-Mihai Burtescu <eddyb@lyken.rs>
2024-11-03 21:09:41 +0200

    Factor out `Node` value inputs from `NodeKind` variants.

--- src/spv/lower.rs
+++ src/spv/lower.rs
@@ -1562,6 +1562,7 @@ impl Module {
                                 &cx,
                                 NodeDef {
                                     attrs: AttrSet::default(),
+                                    inputs: SmallVec::new(),
                                     kind: NodeKind::Block { insts: EntityList::empty() },
                                     outputs: SmallVec::new(),
                                 }
